9d761abb14b803b82656206a73a5022bdba13299
Jose Alberto Moreno Sanchez <jose.moreno@linio.com>
2017-04-30 16:51:44 -0500

    Added navigation passing recipe passing data add elements

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from  '../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from  '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
 		new Recipe('A test Recipe', 'This is simple test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-		new Recipe('A test Recipe', 'This is simple test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+		new Recipe('Another test Recipe', 'This is simple test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
 	];
 
   constructor() { }
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
